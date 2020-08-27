45237b7f90243c864daac8224116ffc65ddffa1c
MartaLourido <martalouri@gmail.com>
2020-08-28 00:41:14 +0200

    some changes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,34 @@ import React from 'react';
 import './App.css';
 import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
-import { Route, Link, Switch } from "react-router-dom";
+import { Route, withRouter, Switch } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css'
 import MyNav from './components/MyNav'
 import axios from 'axios'
-import {API_URL} from './config'
+import { API_URL } from './config'
 import Home from './components/Home'
 
 class App extends React.Component {
 
   state = {
     loggedInUser: null,
+  }
+
+  componentDidMount(){
+    axios.get(`${API_URL}/todos`)
+      .then((res) => {
+          this.setState({
+            todos: res.data
+          })
+      })
+    if (!this.state.loggedInUser){
+      axios.get(`${API_URL}/user`, {withCredentials: true})
+      .then((res) => {
+          this.setState({
+            loggedInUser: res.data
+          })
+      })
+    }  
   }
 
   handleSignIn = (e) => {
@@ -34,31 +51,44 @@ class App extends React.Component {
       })
   }
 
-  handleSignUp = (e) => {
-    e.preventDefault();
-    const { username, email, password } = e.currentTarget;
-
-    axios.post(`${API_URL}/signup`, {
-      username: username.value,
-      email: email.value,
-      password: password.value
-    }, { withCredentials: true })
-      .then((res) => {
-        console.log(res)
+  handleLogOut = (e) => {
+    axios.post(`${API_URL}/logout`, {}, {withCredentials: true})
+      .then(() => {
         this.setState({
-          loggedInUser: res.data
-        }, () => {
+          loggedInUser: null
+        }, ()=>{
           this.props.history.push('/')
         })
       })
   }
 
+  handleSignUp = (e) => {
+    e.preventDefault();
+    const {username, email, password} = e.currentTarget;
+  
+    axios.post(`${API_URL}/signup`, {
+      username: username.value,
+      email: email.value, 
+      password: password.value
+    },  {withCredentials: true})
+      .then((res) => {
+        console.log(res)
+        this.setState({
+          loggedInUser: res.data
+        } ,() => {
+          this.props.history.push('/')
+        })
+      })  
+  }
+
   render() {
     return (
       <div className="App">
+        <h1>Hello. Welcome to Techie</h1>
+          <img src="logo.svg" alt="logo icon" />
         <MyNav loggedInUser={this.state.loggedInUser} onLogout={this.handleLogOut} />
         <Switch >
-        <Route exact path="/" render={(routeProps) => {
+          <Route exact path="/" render={(routeProps) => {
             return <Home {...routeProps} />
           }} />
           <Route path="/SignIn" render={(routeProps) => {
@@ -74,4 +104,4 @@ class App extends React.Component {
 
 }
 
-export default App;
+export default withRouter(App)
--- src/components/MyNav.js
+++ src/components/MyNav.js
@@ -1,92 +1,78 @@
-// import React from 'react'
-// import { Nav, Navbar } from 'react-bootstrap'
-// import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import './MyNav.css';
+// import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBContainer,
+// MDBIcon } from 'mdbreact';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import * as mdb from 'mdb-ui-kit'; // lib
+// import { Input } from 'mdb-ui-kit'; // module
 
-// export default function MyNav(props) {
-//     return (
-//         <div>
-//             {/* <Navbar inverse collapseOnSelect>
-//                 <Navbar.Header>
-//                     <Navbar.Brand>
-//                         <a href="#brand">React-Bootstrap</a>
-//                     </Navbar.Brand>
-//                     <Navbar.Toggle />
-//                 </Navbar.Header>
-//                 <Navbar.Collapse>
-//                     <Nav className="mr-auto">
-//                         <Link to="/">Home</Link>
-//                     </Nav>
-//                     {
-//                         props.loggedInUser ? (
-//                             <Nav className="mr-auto">
-//                                 <button onClick={props.onLogout}>Logout</button>
-//                             </Nav>
-//                         ) : (
-//                                 <>
-//                                     <Nav className="mr-auto">
-//                                         <Link to="/SignIn">Sign In</Link>
-//                                     </Nav>
-//                                     <Nav className="mr-auto">
-//                                         <Link to="/SignUp">Sign Up</Link>
-//                                     </Nav>
-//                                 </>
-//                             )
-//                     }
-//                 </Navbar.Collapse>
-//             </Navbar> */}
-//         </div>
-//     )
+
+// class MyNav extends Component {
+// state = {
+//   collapseID: ''
 // }
 
+// toggleCollapse = collapseID => () => {
+//   this.setState(prevState => ({ collapseID: (prevState.collapseID !== collapseID ? collapseID : '') }));
+// }
 
+// render() {
+//   return (
+//       <MDBContainer>
+//         <MDBNavbar color="green lighten-4" style={{ marginTop: '20px' }} light>
+//           <MDBContainer>
+//             <MDBNavbarBrand>
+//               Techie {/* <img src="logo.png" alt="Logo" width="500" height="600"> */}
+//             </MDBNavbarBrand>
+//             <MDBNavbarToggler tag="button" className="aqua-gradient" onClick={this.toggleCollapse('navbarCollapse13')}>
+//               <span className="white-text">
+//                 <MDBIcon icon="bars" />
+//               </span>
+//             </MDBNavbarToggler>
+//             <MDBCollapse id="navbarCollapse13" isOpen={this.state.collapseID} navbar>
+//               <MDBNavbarNav left>
+//                 <MDBNavItem active>
+//                   <MDBNavLink to="#!">Home</MDBNavLink>
+//                 </MDBNavItem>
+//                 <MDBNavItem>
+//                   <MDBNavLink to="/SignIn">SignIn</MDBNavLink>
+//                 </MDBNavItem>
+//                 <MDBNavItem>
+//                   <MDBNavLink to="/SignUp">SignUp</MDBNavLink>
+//                 </MDBNavItem>
+//               </MDBNavbarNav>
+//             </MDBCollapse>
+//           </MDBContainer>
+//         </MDBNavbar>
+//       </MDBContainer>
 
+//     );
+//   }
+// }
 
-import React, { Component } from 'react';
-import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBContainer,
-MDBIcon } from 'mdbreact';
-import { BrowserRouter as Router } from 'react-router-dom';
+// export default MyNav;
 
-class MyNav extends Component {
-state = {
-  collapseID: ''
-}
+import { slide as Menu } from 'react-burger-menu'
 
-toggleCollapse = collapseID => () => {
-  this.setState(prevState => ({ collapseID: (prevState.collapseID !== collapseID ? collapseID : '') }));
-}
+class MyNav extends React.Component {
+  showSettings (event) {
+    event.preventDefault();
+    
+  }
 
-render() {
-  return (
-      <MDBContainer>
-        <MDBNavbar color="green lighten-4" style={{ marginTop: '20px' }} light>
-          <MDBContainer>
-            <MDBNavbarBrand>
-              Techie {/* <img src="logo.png" alt="Logo" width="500" height="600"> */}
-            </MDBNavbarBrand>
-            <MDBNavbarToggler tag="button" className="aqua-gradient" onClick={this.toggleCollapse('navbarCollapse13')}>
-              <span className="white-text">
-                <MDBIcon icon="bars" />
-              </span>
-            </MDBNavbarToggler>
-            <MDBCollapse id="navbarCollapse13" isOpen={this.state.collapseID} navbar>
-              <MDBNavbarNav left>
-                <MDBNavItem active>
-                  <MDBNavLink to="#!">Home</MDBNavLink>
-                </MDBNavItem>
-                <MDBNavItem>
-                  <MDBNavLink to="/SignIn">SignIn</MDBNavLink>
-                </MDBNavItem>
-                <MDBNavItem>
-                  <MDBNavLink to="/SignUp">SignUp</MDBNavLink>
-                </MDBNavItem>
-              </MDBNavbarNav>
-            </MDBCollapse>
-          </MDBContainer>
-        </MDBNavbar>
-      </MDBContainer>
-
+  render () {
+    // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
+    return (
+      <Menu>
+        <a id="home" className="menu-item" href="/">Home</a>
+        <a id="about" className="menu-item" href="/SignUp">SignUp</a>
+        <a id="contact" className="menu-item" href="/SignIn">SignIn</a>
+        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+      </Menu>
     );
   }
 }
+
+
 
 export default MyNav;
