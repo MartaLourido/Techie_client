421eb22f4a111d2c8035f684552e24a6cca123ac
MartaLourido <martalouri@gmail.com>
2020-08-28 11:37:44 +0200

    signup fixed

--- src/components/MyNav.js
+++ src/components/MyNav.js
@@ -1,78 +1,81 @@
 import React, { Component } from 'react';
-import './MyNav.css';
-// import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBContainer,
-// MDBIcon } from 'mdbreact';
-// import { BrowserRouter as Router } from 'react-router-dom';
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBContainer,
+MDBIcon } from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
 // import * as mdb from 'mdb-ui-kit'; // lib
 // import { Input } from 'mdb-ui-kit'; // module
 
 
-// class MyNav extends Component {
-// state = {
-//   collapseID: ''
-// }
+class MyNav extends Component {
+state = {
+  collapseID: ''
+}
 
-// toggleCollapse = collapseID => () => {
-//   this.setState(prevState => ({ collapseID: (prevState.collapseID !== collapseID ? collapseID : '') }));
-// }
+toggleCollapse = collapseID => () => {
+  this.setState(prevState => ({ collapseID: (prevState.collapseID !== collapseID ? collapseID : '') }));
+}
 
-// render() {
-//   return (
-//       <MDBContainer>
-//         <MDBNavbar color="green lighten-4" style={{ marginTop: '20px' }} light>
-//           <MDBContainer>
-//             <MDBNavbarBrand>
-//               Techie {/* <img src="logo.png" alt="Logo" width="500" height="600"> */}
-//             </MDBNavbarBrand>
-//             <MDBNavbarToggler tag="button" className="aqua-gradient" onClick={this.toggleCollapse('navbarCollapse13')}>
-//               <span className="white-text">
-//                 <MDBIcon icon="bars" />
-//               </span>
-//             </MDBNavbarToggler>
-//             <MDBCollapse id="navbarCollapse13" isOpen={this.state.collapseID} navbar>
-//               <MDBNavbarNav left>
-//                 <MDBNavItem active>
-//                   <MDBNavLink to="#!">Home</MDBNavLink>
-//                 </MDBNavItem>
-//                 <MDBNavItem>
-//                   <MDBNavLink to="/SignIn">SignIn</MDBNavLink>
-//                 </MDBNavItem>
-//                 <MDBNavItem>
-//                   <MDBNavLink to="/SignUp">SignUp</MDBNavLink>
-//                 </MDBNavItem>
-//               </MDBNavbarNav>
-//             </MDBCollapse>
-//           </MDBContainer>
-//         </MDBNavbar>
-//       </MDBContainer>
+render() {
+  return (
+      <MDBContainer>
+        <MDBNavbar color="green lighten-4" style={{ marginTop: '20px' }} light>
+          <MDBContainer>
+            <MDBNavbarBrand>
+              Techie {/* <img src="logo.png" alt="Logo" width="500" height="600"> */}
+            </MDBNavbarBrand>
+            <MDBNavbarToggler tag="button" className="aqua-gradient" onClick={this.toggleCollapse('navbarCollapse13')}>
+              <span className="white-text">
+                <MDBIcon icon="bars" />
+              </span>
+            </MDBNavbarToggler>
+            <MDBCollapse id="navbarCollapse13" isOpen={this.state.collapseID} navbar>
+              <MDBNavbarNav left>
+                <MDBNavItem active>
+                  <MDBNavLink to="#!">Home</MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                  <MDBNavLink to="/SignIn">SignIn</MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                  <MDBNavLink to="/SignUp">SignUp</MDBNavLink>
+                </MDBNavItem>
+              </MDBNavbarNav>
+            </MDBCollapse>
+          </MDBContainer>
+        </MDBNavbar>
+      </MDBContainer>
 
-//     );
-//   }
-// }
-
-// export default MyNav;
-
-import { slide as Menu } from 'react-burger-menu'
-
-class MyNav extends React.Component {
-  showSettings (event) {
-    event.preventDefault();
-    
-  }
-
-  render () {
-    // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
-    return (
-      <Menu>
-        <a id="home" className="menu-item" href="/">Home</a>
-        <a id="about" className="menu-item" href="/SignUp">SignUp</a>
-        <a id="contact" className="menu-item" href="/SignIn">SignIn</a>
-        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
-      </Menu>
     );
   }
 }
 
-
-
 export default MyNav;
+
+
+
+// import React, { Component } from 'react';
+// import './MyNav.css';
+// (import { fallDown as Menu } from 'react-burger-menu'
+
+// class MyNav extends React.Component {
+//   showSettings (event) {
+//     event.preventDefault();
+    
+//   }
+
+//   render () {
+//     // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
+//     return (
+//       <Menu>
+//         <a id="home" className="menu-item" href="/">Home</a>
+//         <a id="about" className="menu-item" href="/SignUp">SignUp</a>
+//         <a id="contact" className="menu-item" href="/SignIn">SignIn</a>
+//         <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+//       </Menu>
+//     );
+//   }
+// }
+
+
+
+// export default MyNav;)
