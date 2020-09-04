4227218d0c99ecf61cf9f4713a584fcd7a49fb79
MartaLourido <martalouri@gmail.com>
2020-09-04 12:43:27 +0200

    deployment

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:5000/api'  //this is for work in local
-// export const API_URL = process.env.REACT_APP_API_URL //this is for do the deployment, heroku
+// export const API_URL = 'http://localhost:5000/api'  //this is for work in local
+export const API_URL = process.env.REACT_APP_API_URL //this is for do the deployment, heroku
