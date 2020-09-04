9bfcf52267e04bd6b8692e845eb910b4ce4b51c9
MartaLourido <martalouri@gmail.com>
2020-09-04 12:28:25 +0200

    changes

--- src/components/CommentPanel.js
+++ src/components/CommentPanel.js
@@ -5,8 +5,9 @@ import ReactLinkify from 'react-linkify'
 import { Button, Comment, Form, Container, Card } from 'semantic-ui-react'
 import moment from 'moment' //for change the format date 
 import ReactPlayer from "react-player" //for be able to share url videos
-import { FacebookShareButton, WhatsappShareButton, LinkedinShareButton, TwitterShareButton, FacebookShareCount, shareUrl } from "react-share" //this is an API for be able to share post or events in social media  
-import { FacebookIcon, WhatsappIcon, LinkedinIcon, TwitterIcon } from "react-share";
+// import { FacebookShareButton, WhatsappShareButton, LinkedinShareButton, LinkedinIcon, FacebookIcon, TwitterShareButton, FacebookShareCount, shareUrl } from "react-share" //this is an API for be able to share post or events in social media  
+
+
 
 import Clock from 'moment'
 
@@ -65,7 +66,7 @@ class CommentPanel extends Component {
         console.log("running showSubComments")
         return (
             <div>
-              
+
                 <div className="comment-list mt-4">
                     <>
                         <Comment.Group>
@@ -119,9 +120,9 @@ class CommentPanel extends Component {
         console.log(doShowSubComments)
         return (
             <div>
-                <span>
+                <div className= "mt-2">
                     <button onClick={() => this.clickComment()} className="btn btn-warning" type="button">Comments</button>
-                </span>
+                </div>
                 {doShowSubComments && this.showSubComments()}
             </div>
         )
@@ -133,40 +134,40 @@ class CommentPanel extends Component {
         const { feed } = this.props
         const { likesCounter } = this.state
         console.log(feed)
-        const shareUrl = "https://www.facebook.com/"
+        let shareUrl = window.location.href;
 
         return (
             <div>
-            <div class="card gedf-card mr-5 mt-5 ml-5">
-                <div class="card-header">
-                    <div class="d-flex justify-content-between align-items-center">
+                <div class="card gedf-card mr-5 mt-5 ml-5">
+                    <div class="card-header">
                         <div class="d-flex justify-content-between align-items-center">
-                            <div class="mr-2">
-                                <img class="rounded-circle" width="45" src={feed.createdby && feed.createdby.userAvatar} alt="" />
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="mr-2">
+                                    <img class="rounded-circle" width="45" src={feed.createdby && feed.createdby.userAvatar} alt="" />
+                                </div>
+                                <div class="ml-2">
+                                    <div class="h5 m-0">{feed.createdby.username}</div>
+                                    <div class="h7 text-muted">{feed.name}</div>
+                                </div>
                             </div>
-                            <div class="ml-2">
-                                <div class="h5 m-0">{feed.createdby.username}</div>
-                                <div class="h7 text-muted">{feed.name}</div>
+                            <div>
+
                             </div>
                         </div>
-                        <div>
-                    
-                        </div>
+
                     </div>
-
                 </div>
-            </div>
 
-             <div class="card-body">
-             <div class="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i> {moment(feed.createAt).format('DD/MM/YYYY')}</div>
-        
-             <p class="card-text">
-             {feed.typeComment === "video" ?
+                <div class="card-body">
+                    <div class="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i> {moment(feed.createAt).format('DD/MM/YYYY')}</div>
+
+                    <p class="card-text">
+                        {feed.typeComment === "video" ?
                             <card>
                                 <ReactPlayer
 
-                                    width="auto%"
-                            
+                                    width="100%"
+
 
                                     url={feed.description}
 
@@ -178,26 +179,38 @@ class CommentPanel extends Component {
                             )
 
                         }
-             </p>
-             
-                     <div class="">
-                       
-                        
-                         <Button onClick={() => this.clickLikes()}
+                    </p>
+
+
+                    <div class="align-center">
+
+                        <div className = "mt-2">
+                        <Button onClick={() => this.clickLikes()}
                             color='red'
+                            
                             content='Like'
                             icon='heart'
                             label={{ basic: true, color: 'red', pointing: 'left', content: <div>{likesCounter}</div> }}
                         />
-                         {this.comments()} 
-                         <FacebookShareCount url={shareUrl} />
-                        
-                         
-                     </div>
-             
-             
-         </div >    
-         </div>
+                        </div>
+                        {this.comments()}
+                        {/* <LinkedinShareButton url={`${shareUrl}`}>
+
+                            <LinkedinIcon size={32} round={true} />
+
+                        </LinkedinShareButton>
+                        <FacebookShareButton url={`${shareUrl}`}>
+
+                            <FacebookIcon size={32} round={true} />
+
+                        </FacebookShareButton>
+                         */}
+
+                    </div>
+
+
+                </div >
+            </div>
 
 
 
